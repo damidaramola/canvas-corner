9a509f1e94b5aed8e37a2bb7cde76454d92f6f60
damidaramola <damidaramola7@gmail.com>
2023-06-24 21:08:19 +0000

    create a component for navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+     
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <Button variant="primary">Hi Dami</Button>
         </p>
         <a
           className="App-link"
