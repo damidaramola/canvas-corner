3266ca3bd565bc33d60060aa4f7c8203f5887bf3
damidaramola <damidaramola7@gmail.com>
2023-06-24 23:11:25 +0000

    remove href links from Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,15 @@ import React from 'react'
 import {Navbar, Container, Nav} from "react-bootstrap"
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" fixed="top">
       <Container>
-    <Navbar.Brand href="#home">CanvasCorner</Navbar.Brand>
+    <Navbar.Brand >CanvasCorner</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
+        <Nav.Link >Home</Nav.Link>
+        <Nav.Link >Sign in</Nav.Link>
+        <Nav.Link >Sign up</Nav.Link>
       </Nav>
     </Navbar.Collapse>
     </Container>
