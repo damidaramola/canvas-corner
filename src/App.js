3031ee49c65033dc2f87751b8d2c322d64fce4e1
damidaramola <damidaramola7@gmail.com>
2023-06-25 22:53:45 +0000

    create route for sign in and home page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@
 import NavBar from './components/NavBar';
 import styles from './App.module.css'
 import Container from 'react-bootstrap/Container'
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
-      <NavBar/>
-        <Container className={styles.Main}>
-          <h1>Home page</h1>
-          <h1> Sign in</h1>
-      
-        </Container>
-    
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Home page</h1>}>
+          </Route>
+          <Route exact path='/signin' render={() => <h1> Sign in</h1>}>
+          </Route>
+        </Switch>
+
+
+      </Container>
+
     </div>
   );
 }
