7f1caa4ef36e9fd9dad165d3fb3886068ca85aa5
damidaramola <damidaramola7@gmail.com>
2023-06-25 22:15:05 +0000

    import container component from bootstrap

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import NavBar from './components/NavBar';
 import styles from './App.module.css'  
 import './index.css'
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
     <div className={styles.App}>
-        <NavBar></NavBar>
+        <NavBar>
+          <Container>
+            
+          </Container>
+        </NavBar>
     </div>
   );
 }
