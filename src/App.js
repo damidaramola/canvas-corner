ab47afdde6faf3c883c1b11d6f87bc8000a8108d
damidaramola <damidaramola7@gmail.com>
2023-07-19 20:24:59 +0000

    move context logic from app.js to currentusercontext.js

--- src/App.js
+++ src/App.js
@@ -15,20 +15,6 @@ export const SetCurrentUserContext = createContext();
 
 function App() {
 
-  const [currentUser, setCurrentUser] = useState(null);
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get('/dj-rest-auth/user/')
-      setCurrentUser(data)
-    } catch (err) {
-      console.log(err)
-    }
-  };
-
-  useEffect(() => {
-    handleMount();
-  }, []);
-
 
   return (
  
