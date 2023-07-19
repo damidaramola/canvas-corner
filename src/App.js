4b63544d1389a987e6f014c262079ce84f0fc480
damidaramola <damidaramola7@gmail.com>
2023-07-19 00:03:38 +0000

    add functionality to check if user is loggedin

--- src/App.js
+++ src/App.js
@@ -6,17 +6,39 @@ import { Route, Switch } from 'react-router-dom';
 import "./api/axiosDefaults";
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null);
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get('/dj-rest-auth/user/')
+      setCurrentUser(data)
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+
   return (
+    <CurrentUserContext>
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
           <Route exact path='/' render={() => <h1>Home page</h1>}></Route>
-          <Route exact path='/signin' render={() => <h1> <SignInForm/></h1>}></Route>
-          <Route exact path='/signup' render={() => <SignUpForm/>}></Route>
-          <Route render={()=> <h1>Page not found!</h1>}/>
+          <Route exact path='/signin' render={() => <h1> <SignInForm /></h1>}></Route>
+          <Route exact path='/signup' render={() => <SignUpForm />}></Route>
+          <Route render={() => <h1>Page not found!</h1>} />
 
         </Switch>
 
@@ -24,6 +46,7 @@ function App() {
       </Container>
 
     </div>
+    </CurrentUserContext>
   );
 }
 
