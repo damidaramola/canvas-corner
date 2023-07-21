8b82390d0a47e529a369ecd706bc3318eac4dc28
damidaramola <damidaramola7@gmail.com>
2023-07-21 19:03:01 +0000

    add state logic which updates state

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 
 import {
-    Alert,
-    Button,
-    Col,
-    Container,
-    Form,
-    Image,
-    Row,
-  } from "react-bootstrap";
+  Alert,
+  Button,
+  Col,
+  Container,
+  Form,
+  Image,
+  Row,
+} from "react-bootstrap";
 
 import Upload from "../../assets/upload.png";
 
@@ -19,32 +19,65 @@ import Asset from "../../components/Asset";
 
 function PostCreateForm() {
 
+  // logic which stores and updates state
   const [errors, setErrors] = useState({});
 
-// this text field holds cancel and create buttons
+  const [postData, setPostData] = useState({
+    title: '',
+    description: '',
+    category: '',
+    image: '',
+  });
+  
+  const { title, description, category, image } = postData;
+
+  const handleChange = (event) =>{
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // this text field holds cancel and create buttons
   const textFields = (
     <div className="text-center">
       <Form.Group>
         <Form.Label>Title</Form.Label>
         <Form.Control
-         type='text' 
-         name='title'
+          type='text'
+          name='title'
         />
       </Form.Group>
       <Form.Group>
         <Form.Label>Description</Form.Label>
         <Form.Control
-         as='textarea' 
-         name='description'
-         rows={6}
+          as='textarea'
+          name='description'
+          rows={6}
         />
       </Form.Group>
+      <Form.Group>
+        <Form.Label>Category</Form.Label>
+        <Form.Control
+          as="select"
+          name="category"
+          className={appStyles.Input}
+          value={category}
+          onChange={handleChange}
+          aria-label="category"
+        >
+          <option>Select artistry level</option>
+          <option value="Novice">Novice</option>
+          <option value="Intermediate">intermediate</option>
+          <option value="Professional">Professional</option>
+        </Form.Control>
+      </Form.Group>
 
-    
-    
+
+
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => {}}
+        onClick={() => { }}
       >
         cancel
       </Button>
@@ -59,18 +92,18 @@ function PostCreateForm() {
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
-            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+            className={`${appStyles.Description} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              
-                <Form.Label
-                  className="d-flex justify-content-center"
-                  htmlFor="image-upload"
-                >
-                  <Asset
+
+              <Form.Label
+                className="d-flex justify-content-center"
+                htmlFor="image-upload"
+              >
+                <Asset
                   src={Upload}
-                  message="Click or tap to upload a picture"/>
-                </Form.Label>
+                  message="Click or tap to upload a picture" />
+              </Form.Label>
 
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
