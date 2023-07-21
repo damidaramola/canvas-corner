a4d957d0ff883ae1aa1997e84557f300ec346895
damidaramola <damidaramola7@gmail.com>
2023-07-21 23:07:17 +0000

    add usereffect logic for post requests

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostPage() {
-  // Add your logic here
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
+    
+    //handles request for post and  will run code
+    //when post id changes in the url
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{data:post}] = await PromiseRejectionEvent.call([
+                    axiosReq.get(`/posts/${id}`)
+                ])
+                setPost({results: [post]})
+                console.log(post)
+            } catch (err) {
+                console.log(err)
+            }
+        };
+      handleMount();  
+    }, [id])
 
+    return (
+        <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                <p>Post component</p>
+                <Container className={appStyles.Content}>
+                    Comments
+                </Container>
+            </Col>
+            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
 
-  return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Post component</p>
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-
-      </Col>
-    </Row>
-  );
+            </Col>
+        </Row>
+    );
 }
 
 export default PostPage;
