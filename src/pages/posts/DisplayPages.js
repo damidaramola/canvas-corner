b29b337023b8484776fb7b6eb56242d111646aa1
damidaramola <damidaramola7@gmail.com>
2023-07-22 23:30:19 +0000

    add no results image

--- src/pages/posts/DisplayPages.js
+++ src/pages/posts/DisplayPages.js
@@ -7,6 +7,10 @@ import Container from "react-bootstrap/Container";
 import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/DisplayPages.module.css";
+import Asset from "../../components/Asset";
+import Post from "./Post";
+import NoResults from '../../assets/no-results.png'
+import { useLocation } from "react-router";
 
 function DisplayPages({message, filter =''}) 
 {
@@ -15,7 +19,7 @@ function DisplayPages({message, filter =''})
   const {pathname} = useLocation();
 
   //makes request to api based on filters
-  
+
   useEffect(()=>{
     const fetchPosts = async ()=>{
       try{
@@ -33,13 +37,27 @@ function DisplayPages({message, filter =''})
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={10}>
-        <p>List of posts here</p>
+        <p>Popular profiles mobile</p>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={2} className="d-none d-lg-block p-0 p-lg-2">
-        add post
-        bookmarked
-        feed
-        <p>categories</p>
+     
       </Col>
     </Row>
   );
