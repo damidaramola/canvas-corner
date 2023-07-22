03fbfa13d30f63a585256a6b5594888e9f7ad53d
damidaramola <damidaramola7@gmail.com>
2023-07-22 23:04:29 +0000

    add code to make request to api through filters

--- src/pages/posts/DisplayPages.js
+++ src/pages/posts/DisplayPages.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
+import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/DisplayPages.module.css";
 
-function DisplayPages() {
+function DisplayPages({message, filter =''}) 
+{
+  const [posts,setPosts] = useState({results:[]});
+  const[hasLoaded,setHasLoaded] = useState(false)
+  const {pathname} = useLocation();
+
+  //makes request to api based on filters
   
+  useEffect(()=>{
+    const fetchPosts = async ()=>{
+      try{
+        const {data} = await axiosReq.get(`/posts/?${filter}`)
+        setPosts(data)
+        setHasLoaded(true)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    setHasLoaded(false)
+    fetchPosts();
+  },[filter,pathname])
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={10}>
