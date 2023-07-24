edd19fc798cc816f96618288838dfb026ed52af5
damidaramola <damidaramola7@gmail.com>
2023-07-24 23:41:01 +0000

    add infinite scroll component to comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -10,6 +10,8 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
 
 function PostPage() {
     const { id } = useParams();
@@ -59,11 +61,20 @@ function PostPage() {
                         "Comments"
                     ) : null}
                     {comments.results.length ? (
-                        comments.results.map(comment => (
-                            <Comment key={comment.id} {...comment}
-                                setPost={setPost}
-                                setComments={setComments} />
-                        ))
+                         <InfiniteScroll
+                         children={comments.results.map((comment) => (
+                           <Comment
+                             key={comment.id}
+                             {...comment}
+                             setPost={setPost}
+                             setComments={setComments}
+                           />
+                         ))}
+                         dataLength={comments.results.length}
+                         loader={<Asset spinner />}
+                         hasMore={!!comments.next}
+                         next={() => fetchMoreData(comments, setComments)}
+                       />
                     ) : currentUser ? (
                         <span>No comments yet, be the first to comment!</span>
                     ) : (
