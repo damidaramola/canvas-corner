3f8a7e191d371ef4bc22746f177b1c842b6be516
damidaramola <damidaramola7@gmail.com>
2023-07-24 23:08:07 +0000

    add logic to hanfle delete function

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -5,29 +5,58 @@ import { Link } from "react-router-dom";
 import Avatar from '../../components/Avatar';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { MenuDropdown } from '../../components/MenuDropdown';
+import { axiosRes } from '../../api/axiosDefaults';
 
 const Comment = (props) => {
-    const { profile_id, profile_image, owner, updated_at, content } = props;
+    const { profile_id,
+        profile_image,
+        owner,
+        updated_at,
+        content,
+        id,
+        setPost,
+        setComments, } = props;
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
-  
+
+    const handleDelete = async () => {
+        try {
+            // makes a delete request to api endpoint
+          await axiosRes.delete(`/comments/${id}/`);
+          setPost((prevPost) => ({
+            results: [
+              {
+                ...prevPost.results[0],
+                comments_count: prevPost.results[0].comments_count - 1,
+              },
+            ],
+          }));
+    
+          setComments((prevComments) => ({
+            ...prevComments,
+            results: prevComments.results.filter((comment) => comment.id !== id),
+          }));
+        } catch (err) {}
+      };
+    
+
     return (
-      <div>
-        <hr />
-        <Media>
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} />
-          </Link>
-          <Media.Body className="align-self-center ml-2">
-            <span className={styles.Owner}>{owner}</span>
-            <span className={styles.Date}>{updated_at}</span>
-            <p>{content}</p>
-          </Media.Body>
-          {is_owner && (<MenuDropdown handleEdit={()=>{}}  handleDelete={()=>{}}/>)}
-        </Media>
-      </div>
+        <div>
+            <hr />
+            <Media>
+                <Link to={`/profiles/${profile_id}`}>
+                    <Avatar src={profile_image} />
+                </Link>
+                <Media.Body className="align-self-center ml-2">
+                    <span className={styles.Owner}>{owner}</span>
+                    <span className={styles.Date}>{updated_at}</span>
+                    <p>{content}</p>
+                </Media.Body>
+                {is_owner && (<MenuDropdown handleEdit={() => { }} handleDelete={handleDelete} />)}
+            </Media>
+        </div>
     );
-  };
-  
-  export default Comment;
+};
+
+export default Comment;
